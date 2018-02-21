9af81737762f1e2b24f6eee7656b0ad1ae82e503
PC <PC@PC-PC>
2018-02-20 20:54:13 -0600

    Trabajando en la tarea

--- src/ipc1/tarea3_201700952/IPC1Tarea3_201700952.java
+++ src/ipc1/tarea3_201700952/IPC1Tarea3_201700952.java
@@ -4,7 +4,8 @@
  * and open the template in the editor.
  */
 package ipc1.tarea3_201700952;
-
+import java.util.Scanner;
+import java.util.InputMismatchException;
 /**
  *
  * @author PC
@@ -15,7 +16,94 @@ public class IPC1Tarea3_201700952 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
-}
+         Scanner leer = new Scanner(System.in);
+        int respuesta;
+        do{
+        System.out.println("MENU PRINCIPAL");
+        System.out.println("1] Usuarios");
+        System.out.println("2] Contador de dígitos");
+        System.out.println("3] Tres numero de mayor a menor");
+        System.out.println("4] Salir\n");
+        
+        System.out.println("INGRESE UNA OPCION\n");
+        respuesta = leer.nextInt();
+
+        try{
+            
+            switch(respuesta){ 
+                case 1:
+                    do{
+                        System.out.println("USUARIOS");
+                        System.out.println("1] Ingresar Usuarios");
+                        System.out.println("2] Mostrar usuarios ascendente");
+                        System.out.println("3] Mostrar usuarios descendente");
+                        System.out.println("0] Menu principal\n");
+                        System.out.println("Ingrese una opcion\n");
+                        
+                        respuesta = leer.nextInt();
+                        //System.out.println("\n");
+                        try{
+                            switch(respuesta){
+                                case 0:
+                                    break;
+                                case 1:
+                                    System.out.println("Ingrese su usuario\n");
+                                    break;
+                                case 2:
+                                    System.out.println("Usuarios en forma ascendente\n");
+                                    break;
+                                case 3:
+                                    System.out.println("Usuarios en forma descendente\n");
+                                    break;
+                                default:
+                                    System.out.println("Ingrese una opcion correcta\n");
+                            }
+                        }catch(Exception e){
+                        System.out.println("Ingrese una opcion correcta");
+                        leer.next();
+                        }
+                    }while(respuesta !=0);
+                    break;
+                case 2:
+                    do{
+                        System.out.println("CONTADOR DE DIGITOS");
+                        System.out.println("1] Ingresar numero");
+                        System.out.println("2] Mostrar numero de digitos");
+                        System.out.println("0] Menu principal\n");
+                        System.out.println("Ingrese una opcion\n");
+                        
+                        respuesta = leer.nextInt();
+                        try{
+                            switch(respuesta){
+                                case 0:
+                                    break;
+                                case 1:
+                                    System.out.println("Ingres un numero\n");
+                                    break;
+                                case 2:
+                                    System.out.println("Mostrar el número de "
+                                            + "digitos del numero ingresado\n");
+                                    break;
+                                default:
+                                    System.out.println("Ingrese una opcion correcta\n");
+                            }
+                        }catch(Exception e){
+                        System.out.println("Ingrese una opcion correcta");
+                        leer.next();
+                        }
+                    }while(respuesta !=0);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+            }catch(Exception e){
+                System.out.println("Ingrese una opcion correcta");
+                leer.next();
+            }
+        }while(respuesta !=4);
+    }     
+} 
